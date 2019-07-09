4e450c25f6e21b87f9d523bad6e07e2b4b34cb88
Lazigerbill <chaukbill@gmail.com>
2019-07-09 15:38:58 -0400

    jul 9 project created

---
+++ jul9.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  9 15:30:52 2019
+
+@author: localadmin
+"""
+
+def my_function(country):
+    print("I am from " + country)
+    
+country = input("where are you from?")
+my_function(country)
